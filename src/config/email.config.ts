8900f41e306ff3b2bdefb1fac39372341d00f8a1
Petr Slavík <petrslavik@Petr--MacBook-Pro-5.local>
2026-01-13 20:49:59 +0100

    fix: update EmailJS contact form template ID

---
+++ src/config/email.config.ts
@@ -0,0 +1,11 @@
+export const emailConfig = {
+    publicKey: 'oH75QBxPioG8wjGwj',
+    form: {
+        serviceId: 'service_7pryfsp',
+        templateId: 'template_vh4czst',
+    },
+    auth: {
+        serviceId: 'service_izixwz7',
+        templateId: 'template_reset_password', // Placeholder
+    }
+};
